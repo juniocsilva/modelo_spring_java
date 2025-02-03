3e8be7a4c8b88f660ff3fd15e740de7508f64b2f
juniocsilva <35564723+juniocsilva@users.noreply.github.com>
2025-02-03 16:43:47 -0300

    Commit do MAC

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,7 @@
-SET IDENTITY_INSERT fabricante ON
+--SET IDENTITY_INSERT fabricante ON
 insert into fabricante (id, nome) values (1, 'VOLKSWAGEN');
 insert into fabricante (id, nome) values (2, 'GM');
-SET IDENTITY_INSERT fabricante OFF
+--SET IDENTITY_INSERT fabricante OFF
 
 insert into produto (nome, preco, fabricante_id) values ('FUSCA', 2000.07, 1);
 insert into produto (nome, preco, fabricante_id) values ('OPALA', 3000.40, 2);
